ef171063ed74ae60e10fce1dce4c95a391c6c398
dragnot <guy.lubovitch@gmail.com>
2012-09-24 16:02:46 +0200

    add a glocal counter to the common project

---
+++ common/src/main/java/org/openspaces/bigdata/common/counters/GlobalCounter.java
@@ -0,0 +1,47 @@
+package org.openspaces.bigdata.common.counters;
+
+import com.gigaspaces.annotation.pojo.SpaceId;
+import com.gigaspaces.annotation.pojo.SpaceRouting;
+
+public class GlobalCounter {
+
+	private String token;
+
+    @SpaceId(autoGenerate = false)
+    @SpaceRouting
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	private Integer count;
+ 
+	
+	public GlobalCounter(String token, int counter) {
+
+		this.token = token;
+		this.count = counter;
+		
+	}
+	public GlobalCounter(){}
+	
+	public void inceamentCount() {
+		// TODO Auto-generated method stub
+		this.count++;
+	}
+
+	
+	
+	
+}
